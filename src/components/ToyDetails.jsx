f75e33dce514b167c149862a1472ce0df5feb7de
btamit <bishojittoppo118@gmail.com>
2023-05-21 05:09:51 +0600

    toy details added

--- src/components/ToyDetails.jsx
+++ src/components/ToyDetails.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ToyDetails = () => {
+    const {id} = useParams();
+    const [toyDetails, setToyDetails] = useState({});
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/toyDetails/${id}`)
+        .then(res => res.json())
+        .then(data => setToyDetails(data))
+    },[id])
+
     return (
-        <div>
-            <h2 className='text-5xl font-bold text-center'>Welcome to toy details page</h2>
-        </div>
+      <div className="bg-zinc-500 p-5 rounded flex flex-col">
+            <h2>Welcome to toy details page {toyDetails.price} </h2>
+      </div>
     );
 };
 
